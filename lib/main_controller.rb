7b2c1a1a1913dac6f520835398118ecb0c8ed91c
tochman <thomas@nocebo.se>
2015-10-01 23:09:34 +0200

    added foundation and refactored some methods

--- lib/main_controller.rb
+++ lib/main_controller.rb
@@ -1,6 +1,14 @@
 require 'sinatra/base'
+require 'sinatra/form_helpers'
+require 'sinatra/flash'
+require 'tilt/erb'
+require 'data_mapper'
 
 class Weather < Sinatra::Base
+
+  helpers Sinatra::FormHelpers
+  register Sinatra::Flash
+  
   get '/' do
     erb :index
   end
